7f7e0038635289f1214cd4751839787cda7883f9
Dennis Kats <denk0403@gmail.com>
2019-07-23 00:19:58 -0400

    Added support for companion-to-watch communication

---
+++ companion/index.js
@@ -0,0 +1,37 @@
+import * as messaging from "messaging";
+import { settingsStorage } from "settings";
+
+// Message socket opens
+messaging.peerSocket.onopen = () => {
+  restoreSettings();
+};
+
+// A user changes settings
+settingsStorage.onchange = evt => {
+  let data = {
+    key: evt.key,
+    newValue: evt.newValue
+  };
+  sendVal(data);
+};
+
+// Restore any previously saved settings and send to the device
+function restoreSettings() {
+  for (let index = 0; index < settingsStorage.length; index++) {
+    let key = settingsStorage.key(index);
+    if (key) {
+      let data = {
+        key: key,
+        newValue: settingsStorage.getItem(key)
+      };
+      sendVal(data);
+    }
+  }
+}
+
+// Send data to device using Messaging API
+function sendVal(data) {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    messaging.peerSocket.send(data);
+  }
+}
