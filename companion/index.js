c9a16701baf233461d40517a84e2224059aa09da
Dennis Kats <denk0403@gmail.com>
2019-08-12 05:12:41 -0400

    Added request to wake companion every 5 minutes

--- companion/index.js
+++ companion/index.js
@@ -1,5 +1,9 @@
 import * as messaging from "messaging";
 import { settingsStorage } from "settings";
+import { me } from "companion"
+
+const MS_PER_MIN = 1000 * 60;
+me.wakeInterval = 5 * MS_PER_MIN;
 
 // Message socket opens
 messaging.peerSocket.onopen = () => {
